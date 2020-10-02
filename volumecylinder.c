1836bf296a06a6c851ee01de74b2bf09f84b4f9a
Stark1392 <72224947+Stark1392@users.noreply.github.com>
2020-10-02 10:01:06 +0530

    Add files via upload

---
+++ volumecylinder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int radius,height;
+printf("What is the radius of the cylinder \n");
+scanf("%d", &radius);
+printf("What is the height of the cylinder\n");
+scanf("%d", &height);
+printf("The volumeof cylinder is %d \n", radius*height);
+	return 0;
+}
