41920449ac2d5606ff12eb929eb46e77bd6713a7
Stark1392 <72224947+Stark1392@users.noreply.github.com>
2020-10-02 09:57:08 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int radius;
+printf("What is the radius of the circle \n");
+scanf("%d", &radius);
+printf("The area of circle is %d \n", radius*radius);
+	return 0;
+}
