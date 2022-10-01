f638bf8fbe6c9c15640ca87cfe01f65bfbfa68c1
Hardik Patel <hardik20507@iiitd.ac.in>
2022-10-01 10:06:29 +0530

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,5 +1,7 @@
+// This is how you give output or rather a print statement in C programming.
 #include<stdio.h>
 int main(){
 	printf("Hello World, It's Hardik");
+	printf("This is how you print output on screen or to cmd command line")
 	return 0;
 }
